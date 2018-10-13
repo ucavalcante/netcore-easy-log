d7d7067e834e1ee55e2d41e83709498a56ba4dab
ucavalcante <ziel7@hotmail.com>
2018-10-13 14:28:01 -0300

    Insert using and function mechanics

--- src/ncel/Logs.cs
+++ src/ncel/Logs.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace ncel
 {
     class Logs
     {
-        public static void Information(string msgToLogToLog)
+        public static void Information(string msgToLog)
         {
             string sDiretorioLog = Directory.GetCurrentDirectory() + "\\Logs";
-
+            //ToDo Create a config method
+            var cfg = new {
+                DiretorioLogs = Directory.GetCurrentDirectory() + "\\Logs"
+            };
+            //ToDo Create a way to Get Previous caller Method
+            var local = "InConstruction";
             DirectoryInfo log = new DirectoryInfo(cfg.DiretorioLogs);
             if (log.Exists)
             {
                 sDiretorioLog = log.FullName;
             }
-
             try
             {
                 var line = $"[{DateTime.Now}][INFO][{local}][{msgToLog}]";
@@ -22,14 +28,13 @@ namespace ncel
                 {
                     diretoriolog.Create();
                 }
-                using (StreamWriter sw = File.AppendText(diretoriolog + "\\" + "Log_" + Process.GetCurrentProcess().ProcessName + "_" + DateTime.Now.ToString("yyyy_MM_dd") + ".log"))
+                using (StreamWriter sw = File.AppendText($"{diretoriolog}\\Log_{Process.GetCurrentProcess().ProcessName}_{DateTime.Now.ToString("yyyy_MM_dd")}.log"))
                 {
                     sw.WriteLine(line);
                 }
             }
             catch (Exception ex)
             {
-
                 DirectoryInfo diretoriolog = new DirectoryInfo(DefaultLogFilePath);
                 if (!diretoriolog.Exists)
                 {
@@ -50,10 +55,9 @@ namespace ncel
                     sw.WriteLine("--------------------------------");
                     sw.WriteLine();
                 }
-
-
             }
         }
-        public static readonly string DefaultLogFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        public static readonly string DefaultLogFilePath = System.IO.Path.GetTempPath();
+        // Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
     }
 }
